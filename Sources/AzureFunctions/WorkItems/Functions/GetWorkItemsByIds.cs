ffe35506b281549cd1ac9d57666cb64d77f85d3d
Matthias Mueller <matthiasm@live.de>
2018-10-22 22:10:33 +0200

    WIP

--- Sources/AzureFunctions/WorkItems/Functions/GetWorkItemsByIds.cs
+++ Sources/AzureFunctions/WorkItems/Functions/GetWorkItemsByIds.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Mmu.Mlazh.TfsProxy.Application.Common.Infrastructure.AzureFiles.Services;
 using Mmu.Mlazh.TfsProxy.Application.WorkItems.Areas.App.DtoModeling.Services;
 using Mmu.Mlazh.TfsProxy.AzureFunctions.Common.Infrastructure.ServiceProvisioning;
 using Mmu.Mlazh.TfsProxy.Dependencies;
@@ -15,19 +16,30 @@ namespace Mmu.Mlazh.TfsProxy.AzureFunctions.WorkItems.Functions
     public static class GetWorkItemsByIds
     {
         [FunctionName("GetWorkItemsByIds")]
-        public static async Task<IActionResult> GetAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = "GetWorkItemsById/workItemIds")] HttpRequest req, ILogger logger, string workItemIds)
+        public static async Task<IActionResult> GetAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetWorkItemsById")] HttpRequest req, ILogger logger)
         {
-            DependenciesProvider.ProvideDependencencies();
-            var workItemDtoDataService = AppProvisioningService.GetService<IWorkItemDtoDataService>();
+            try
+            {
+                DependenciesProvider.ProvideDependencencies();
+                var workItemDtoDataService = AppProvisioningService.GetService<IWorkItemDtoDataService>();
 
-            var workitemIdsSplitted = workItemIds
-                .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
+                var workItemIds = req.Query["workitemIds"].First();
 
-            var result = await workItemDtoDataService.LoadByIdsAsync(workitemIdsSplitted);
+                var workitemIdsSplitted = workItemIds
+                    .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
 
-            return new OkObjectResult(result);
+                var result = await workItemDtoDataService.LoadByIdsAsync(workitemIdsSplitted);
+
+                return new OkObjectResult(result);
+            }
+            catch (Exception ex)
+            {
+                var fileService = AppProvisioningService.GetService<IFileService>();
+                await fileService.AppendAsync(ex.Message + ex.StackTrace);
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
