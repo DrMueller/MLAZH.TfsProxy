2b8374140e9486022f8c8e02936df57ed5910b40
Matthias Mueller <matthiasm@live.de>
2018-10-23 10:16:08 +0200

    WIP

--- Sources/AzureFunctions/Common/Areas/TestFunction.cs
+++ Sources/AzureFunctions/Common/Areas/TestFunction.cs
@@ -1,10 +1,8 @@
-using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Build.Framework;
-using Newtonsoft.Json;
 
 namespace Mmu.Mlazh.TfsProxy.AzureFunctions.Common.Areas
 {
@@ -13,15 +11,7 @@ namespace Mmu.Mlazh.TfsProxy.AzureFunctions.Common.Areas
         [FunctionName("TestFunction")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
-            string name = req.Query["name"];
-
-            var requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkObjectResult($"Hello, World");
         }
     }
 }
--- Sources/AzureFunctions/WorkItems/Functions/PostWorkItem.cs
+++ Sources/AzureFunctions/WorkItems/Functions/PostWorkItem.cs
@@ -1,31 +1,31 @@
-﻿using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Mmu.Mlazh.TfsProxy.Application.WorkItems.Areas.App.DtoModeling.Dtos;
-using Mmu.Mlazh.TfsProxy.Application.WorkItems.Areas.App.DtoModeling.Services;
-using Mmu.Mlazh.TfsProxy.AzureFunctions.Common.Infrastructure.ServiceProvisioning;
-using Mmu.Mlazh.TfsProxy.Dependencies;
-using Newtonsoft.Json;
+﻿//using System.IO;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Extensions.Http;
+//using Mmu.Mlazh.TfsProxy.Application.WorkItems.Areas.App.DtoModeling.Dtos;
+//using Mmu.Mlazh.TfsProxy.Application.WorkItems.Areas.App.DtoModeling.Services;
+//using Mmu.Mlazh.TfsProxy.AzureFunctions.Common.Infrastructure.ServiceProvisioning;
+//using Mmu.Mlazh.TfsProxy.Dependencies;
+//using Newtonsoft.Json;
 
-namespace Mmu.Mlazh.TfsProxy.AzureFunctions.WorkItems.Functions
-{
-    public static class PostWorkItem
-    {
-        [FunctionName("PostWorkItem")]
-        public static async Task<IActionResult> PostAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
-        {
-            DependenciesProvider.ProvideDependencencies();
+//namespace Mmu.Mlazh.TfsProxy.AzureFunctions.WorkItems.Functions
+//{
+//    public static class PostWorkItem
+//    {
+//        [FunctionName("PostWorkItem")]
+//        public static async Task<IActionResult> PostAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
+//        {
+//            DependenciesProvider.ProvideDependencencies();
 
-            var requestBody = new StreamReader(req.Body).ReadToEnd();
-            var postWorkItemDto = JsonConvert.DeserializeObject<PostWorkItemDto>(requestBody);
+//            var requestBody = new StreamReader(req.Body).ReadToEnd();
+//            var postWorkItemDto = JsonConvert.DeserializeObject<PostWorkItemDto>(requestBody);
 
-            var workItemDtoDataService = AppProvisioningService.GetService<IWorkItemDtoDataService>();
+//            var workItemDtoDataService = AppProvisioningService.GetService<IWorkItemDtoDataService>();
 
-            var result = await workItemDtoDataService.PostAsync(postWorkItemDto);
-            return new OkObjectResult(result);
-        }
-    }
-}
+//            var result = await workItemDtoDataService.PostAsync(postWorkItemDto);
+//            return new OkObjectResult(result);
+//        }
+//    }
+//}
